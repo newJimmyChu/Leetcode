87f74f5effe9831574eccb778f5f22f43024aa91
newJimmyChu <zpc5077@psu.edu>
2020-08-26 22:10:52 +0800

    add new files

---
+++ tencent/Arrays/two_sum.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+        import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            if (hash.containsKey(target - nums[i]))
+                return new int[]{hash.get(target - nums[i]), i};
+            hash.put(nums[i], i);
+        }
+        return new int[2];
+    }
+}
